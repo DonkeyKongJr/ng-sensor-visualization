085874a029c5358769d3bebd501a1ec0983ff04c
Patrick Schadler <patrick.schadler@gmail.com>
2018-07-12 21:04:08 +0200

    feat(firebase): configuration
    add basic configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,13 @@
 
 export const environment = {
   production: false,
-  sensorDataUrl: 'http://192.168.1.43:3000'
+  sensorDataUrl: 'http://192.168.1.43:3000',
+  firebase: {
+    apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
